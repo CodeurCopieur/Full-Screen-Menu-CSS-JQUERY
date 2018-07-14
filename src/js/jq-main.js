f5e4264e7b7ea164b52685393a8537b3550f316b
codwerk <louis_widdy@live.fr>
2018-07-14 11:26:00 +0200

    nv commit reprise

--- src/js/jq-main.js
+++ src/js/jq-main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $("#main").click(function(){
-        $("#main").animate({height:'50em',width:'50em'}); /* augmentation de la hauteur et largeur */
+        $("#main").animate({height:'800px',width:'800px'}); /* augmentation de la hauteur et largeur */
         $("#icon").fadeOut();
         $("#menu ul").delay(300).animate({opacity:"1"});
         $("#menu ul").css("visibility","visible");
+        $("#menu_circle_layer").animate({height:'850px',width:'850px'});
     });
 });
